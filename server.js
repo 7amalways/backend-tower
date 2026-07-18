3c8cf7630b3809ea3468a6694dc2602c8182f87d
7amalways <victordesouzavilasboas@gmail.com>
2026-07-18 19:17:10 -0300

    Refactor server.js for improved response and port setup
    Updated server response message and port configuration.

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-console.log("TESTE FUNCIONANDO");
-
 const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Servidor OK");
+  res.send("Backend online 🔥");
 });
 
-app.listen(3000, () => {
-  console.log("Servidor rodando na porta 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor rodando");
 });

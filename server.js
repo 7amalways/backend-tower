d08fd7533512e05a28e34ffef687dd71a48ccb60
7amalways <victordesouzavilasboas@gmail.com>
2026-07-18 19:10:24 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+console.log("TESTE FUNCIONANDO");
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Servidor OK");
+});
+
+app.listen(3000, () => {
+  console.log("Servidor rodando na porta 3000");
+});
